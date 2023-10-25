fa8e6cb2793597eef0dbba44a2566907d969c3e4
leirdan <uouleirdan@gmail.com>
2023-10-25 09:23:14 -0300

    :sparkles: feat: added models

---
+++ Metallify.API/Models/Album.cs
@@ -0,0 +1,8 @@
+﻿namespace Metallify.API.Models;
+
+public class Album
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Genre Genre { get; set; }
+}
---
+++ Metallify.API/Models/Genre.cs
@@ -0,0 +1,7 @@
+﻿namespace Metallify.API.Models;
+
+public class Genre
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
