882d2eed69bcef07f0e399b66dee43d26adedafe
leirdan <uouleirdan@gmail.com>
2023-10-25 09:32:14 -0300

    :test_tube: tests: add some code on controller to approve a test

--- Metallify.API/Controllers/AlbumsController.cs
+++ Metallify.API/Controllers/AlbumsController.cs
@@ -13,9 +13,11 @@ public class AlbumsController : ControllerBase
         _albumService = a;
     }
 
-    [HttpGet(Name = "GetAlbum")]
+    [HttpGet(Name = "GetAlbums")]
     public async Task<IActionResult> Get()
     {
+        // now, the test 2nd test will work
+        var albums = _albumService.GetAllAlbums();
         return Ok("all good");
     }
 }
--- Metallify.Tests/Systems/Controllers/TestAlbumsController.cs
+++ Metallify.Tests/Systems/Controllers/TestAlbumsController.cs
@@ -19,7 +19,7 @@ public class TestAlbumsController
     }
 
     [Fact]
-    // this one is failing because it's on the "red" phase of "red/green/refactor" TDD
+    // now this one works: it's on "green" phase on "red/green/refactor"
     public async void Get_OnSuccess_InvokeAlbumServiceExactlyOnce()
     {
         // Arrange
