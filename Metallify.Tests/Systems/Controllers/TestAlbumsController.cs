eb41225df41d6a9b890c910b470993e4188c4684
leirdan <uouleirdan@gmail.com>
2023-10-25 13:18:18 -0300

    :white_check_mark: test: added 2 tests and refactored 1

--- Metallify.Tests/Systems/Controllers/TestAlbumsController.cs
+++ Metallify.Tests/Systems/Controllers/TestAlbumsController.cs
@@ -13,7 +13,19 @@ public class TestAlbumsController
     // this one is okay
     public async void Get_OnSuccess_ReturnsStatusCode200()
     {
-        var c = new AlbumsController(new Mock<IAlbumService>().Object);
+        var mock = new Mock<IAlbumService>();
+        mock
+            .Setup(s => s.GetAllAlbums())
+            .ReturnsAsync(new List<Album>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Title = "Animals",
+                    Genre = new () { Name = "Progressive Rock", Description = "Amazing!"}
+                }
+            });
+        var c = new AlbumsController(mock.Object);
         var result = await c.Get() as OkObjectResult;
         result.StatusCode.Should().Be(200);
     }
@@ -29,8 +41,55 @@ public class TestAlbumsController
             .ReturnsAsync(new List<Album>());
         var c = new AlbumsController(mock.Object);
         // Act
-        var result = await c.Get() as OkObjectResult;
+        var result = await c.Get();
         // Assert
         mock.Verify(s => s.GetAllAlbums(), Times.Once());
+    }
+
+    [Fact]
+    public async void Get_OnSuccess_ReturnsListOfAlbums()
+    {
+        // Arrange
+        var mock = new Mock<IAlbumService>();
+        mock
+            .Setup(s => s.GetAllAlbums())
+            .ReturnsAsync(new List<Album>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Title = "Animals",
+                    Genre = new () { Name = "Progressive Rock", Description = "Amazing!"}
+                }
+            });
+
+        var c = new AlbumsController(mock.Object);
+
+        // Act
+        var result = await c.Get();
+        var obj = result as OkObjectResult;
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+        obj.Value.Should().BeOfType<List<Album>>();
+    }
+
+    [Fact]
+    public async void Get_OnNoAlbumsFound_ReturnsStatusCode404()
+    {
+        // Arrange
+        var mock = new Mock<IAlbumService>();
+        mock
+            .Setup(s => s.GetAllAlbums())
+            .ReturnsAsync(new List<Album>());
+
+        var c = new AlbumsController(mock.Object);
+
+        // Act
+        var result = await c.Get();
+        var obj = result as NotFoundObjectResult;
+
+        // Assert
+        result.Should().BeOfType<NotFoundResult>();
     }
 }
